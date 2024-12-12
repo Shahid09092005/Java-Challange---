f575bcea262a667366b3d33392465aff0ead4806
Shahid Mansuri <140784065+Shahid09092005@users.noreply.github.com>
2024-12-12 17:34:47 +0530

    509. Fibonacci Number

--- DP-1/509_leetcode_Easy.java
+++ DP-1/509_leetcode_Easy.java
@@ -1,1 +1,26 @@
- 
+class Solution { 
+    // 
+    public int funct(int n,int[] dp){
+        if(n==0 || n==1){ //Base case
+            return dp[n];
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        // Calculate and store the value in the dp array
+        dp[n] = funct(n - 1, dp) + funct(n - 2, dp);
+        return dp[n];
+    }
+    public int fib(int n) {
+         // Initialize the dp array with -1
+        int[] dp = new int[30+1];
+        for (int i = 0; i <= n; i++) {
+            dp[i] = -1;
+        }
+         // Base values
+        dp[0]=0;
+        dp[1]=1;
+        return funct(n,dp);
+
+    }
+}
