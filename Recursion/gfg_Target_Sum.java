4483bb436f6f7dd357018361feac001fe6c617f5
Shahid Mansuri <140784065+Shahid09092005@users.noreply.github.com>
2024-12-15 01:57:12 +0530

    gfg Target Sum Subsequence

--- Recursion/gfg_Target_Sum.java
+++ Recursion/gfg_Target_Sum.java
@@ -1,16 +1,16 @@
                                                 // Time limit exceed for larget size of the array
                                                 // See dp solution
-public class Solution {
-    //If there exist a subset then return 1 else return 0.
-    public int check(int[] arr  , int t ,int idx , int sos){
-        if(sos==t) return 1;
-        if(idx>=arr.length) return 0;
-        check(arr,t,idx+1,sos+arr[idx]);
-        check(arr,t,idx+1,sos);
-        return 0;
+class Solution {
+    static Boolean check(int[] arr ,int t,int idx,int sos){
+        if(sos==t) return true;
+        if(idx>=arr.length) return false;
+        if(check(arr,t,idx+1,sos+arr[idx])) return true ; //include
+        if(check(arr,t,idx+1,sos)) return true; // not incluse
+        return false;
     }
-    public int solve(int[] A, int B) {
-        // A is Array and B is target
-        return check( A , B ,0,0);
+
+    static Boolean isSubsetSum(int arr[], int target) {
+        return check(arr,target,0,0);
+        
     }
 }
